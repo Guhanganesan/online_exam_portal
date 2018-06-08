2cb4b6e14c5ca9d66d5bd4e5238887e23cdc09fb
Guhan Ganesan <bharadhiguhan@ymail.com>
2018-06-08 15:38:23 +0530

    Update Db.php

--- Db.php
+++ Db.php
@@ -1,9 +1,9 @@
 <?php
 	
      $host="localhost";
-     $user="id3242175_guha_5599";
-     $pass="guha0804";
+     $user="";
+     $pass="";
      
-     $con=mysqli_connect($host,$user,$pass, "id3242175_guha");
+     $con=mysqli_connect($host,$user,$pass, "");
      
 ?>
